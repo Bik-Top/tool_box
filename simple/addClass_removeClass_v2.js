d84bb48b78be78b58d38809bca507ebb003cb6c9
BikTop <Bik-Top@users.noreply.github.com>
2014-10-21 17:07:46 +0300

    Update addClass_removeClass_v2.js

--- simple/addClass_removeClass_v2.js
+++ simple/addClass_removeClass_v2.js
@@ -33,16 +33,3 @@ function removeClass(o, c){
 
 
 
-
-
-
-function addClass(o, c) {
-    var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g");
-    if (re.test(o.className)) return false ;
-    o.className = (o.className + " " + c).replace(/\s+/g, " ").replace(/(^ | $)/g, "");
-}
-
-function removeClass(o, c) {
-    var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g");
-    o.className = o.className.replace(re, "$1").replace(/\s+/g, " ").replace(/(^ | $)/g, "");
-}
