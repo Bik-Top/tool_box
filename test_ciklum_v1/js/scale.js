9baa60c6bbe0f45693377fdfa38a5549f45188c4
BikTop <mantorovv@gmail.com>
2014-11-16 16:38:49 +0200

    temp1

--- test_ciklum_v1/js/scale.js
+++ test_ciklum_v1/js/scale.js
@@ -11,8 +11,15 @@ window.onload = (function () {
   append(open_var.wrapping);
   open_var.wrapping.appendChild(open_var.imgPlace);
   open_var.imgPlace.appendChild(open_var.ig);
+  open_var.imgPlace.appendChild(open_var.button_download);
+  append(open_var.headerBar);
+  append(open_var.footerBar);
+  open_var.footerBar.appendChild(open_var.button_like);
+  open_var.footerBar.appendChild(open_var.button_dislike);
+  open_var.footerBar.appendChild(open_var.button_share);
+  open_var.footerBar.appendChild(open_var.button_close);
   //append(open_var.footerBar);
-  resizeGame();
+  resizer();
 });
 window.open_var = {
   wrapping: newElem('div', {
@@ -25,26 +32,49 @@ window.open_var = {
   }),
   headerBar: newElem('div', {
     id: 'headerBar',
-    style: 'position: absolute; width: 100%; height: 8%; top: 0; opacity: 0.8;'
+    style: 'position: absolute; width: 100%; height: 8%; top: 0; '
   }),
   ig: newElem('img', {
-    src: '',//'https://fbcdn-photos-g-a.akamaihd.net/hphotos-ak-xpa1/t39.2082-0/p528x396/851538_506458892778613_1885463861_n.jpg',
+    src: 'https://fbcdn-photos-g-a.akamaihd.net/hphotos-ak-xpa1/t39.2082-0/p528x396/851538_506458892778613_1885463861_n.jpg',
     style: ' width: 100%; height: 100%;'
   }),
   footerBar: newElem('div', {
     class: '',
     id: 'footerBar',
-    style: 'position: absolute; width: 100%; height: 8%;bottom: 0;opacity: 0.8;'
+    style: 'position: absolute; width: 100%; bottom: 0;text-align: center;'
   }),
   footerBarVert: newElem('div', {
     class: '',
     id: 'footerBar',
-    style: 'position: absolute; height: 100%; width: 8%;left: 0;top:0;;'
+    style: 'position: absolute; height: 100%; width: 8%;left: 0;top:0;'
+  }),
+  button_download: newElem('div', {
+    class: '',
+    id: 'button_download',
+    style: ' height: 8%; width: 100%;background:#F00;'
+  }),
+  button_close: newElem('span', {
+    class: '',
+    id: 'button_close',
+    style: 'display: inline-block; background:url(img/close.png); background-size: 100%;'
+  }),
+  button_like: newElem('span', {
+    class: '',
+    id: 'button_like',
+    style: 'display: inline-block; background:url(img/like.png); background-size: 100%;'
+  }),
+  button_dislike: newElem('span', {
+    class: '',
+    id: 'button_dislike',
+    style: 'display: inline-block; background:url(img/dislike.png); background-size: 100%;'
+  }),
+  button_share: newElem('span', {
+    class: '',
+    id: 'button_share',
+    style: 'display: inline-block; background:url(img/share.png); background-size: 100%;'
   }),
   widthToHeight: 4 / 3
 };
-
-
 function newElem(tag, params) {
   params = params || {};
   var elem = document.createElementNS ?
@@ -55,7 +85,6 @@ function newElem(tag, params) {
   }
   return elem;
 }
-
 function attr(el, at, value) {
   at = {'for': 'htmlFor', 'class': 'className'}[at] || at;
   if (!value) {
@@ -69,73 +98,63 @@ function attr(el, at, value) {
     if (el.setAttribute)   el.setAttribute(at, value);
   }
 }
-
 function append(el, where) {
   (where || document.body).appendChild(el);
 }
-
 function remove(el) {
-   el.parentNode.removeChild(el);
-  console.log('el'+el);
+  el.parentNode.removeChild(el);
+  console.log('el' + el);
 }
-
-
-function resizeGame() {
+function resizer() {
   var newWidth,
-    newHeight,
-    newWidthToHeight,coefWidth,coefHeight;
-
-
-  coefWidth = (window.innerWidth*16)/100;
-  coefHeight = (window.innerHeight*16)/100;
-  newWidth =  window.innerWidth;
-  newHeight =  window.innerHeight;
+    newHeight, hh,
+    hFooter,
+    newWidthToHeight;
+  hh = (open_var.footerBar.offsetHeight + open_var.headerBar.offsetHeight);
+  hFooter = open_var.footerBar.offsetHeight;
+  newWidth = window.innerWidth;
+  newHeight = window.innerHeight;
   newWidthToHeight = newWidth / newHeight;
-  if (newWidthToHeight > open_var.widthToHeight) { //landscape
+  if (newWidthToHeight > open_var.widthToHeight) {
+    //console.log('landscape'); //landscape
     newWidth = newHeight * open_var.widthToHeight;
-    open_var.wrapping.style.height = newHeight-coefHeight + 'px';
-    open_var.wrapping.style.width = newWidth-coefWidth + 'px';
-
-
-  } else {                                         //portrait
+    open_var.wrapping.style.width = Math.floor(newWidth - hh - hFooter) + 'px';
+    //open_var.wrapping.style.height = Math.floor(newHeight - hh - hFooter) + 'px';
+    //open_var.imgPlace.style.width = Math.floor(newWidth - hh - hFooter) + 'px';
+    open_var.imgPlace.style.height = Math.floor(newHeight - hh - hFooter) + 'px';
+    open_var.wrapping.style.marginLeft = Math.floor((-newWidth + hh) / 2) + 'px';
+    open_var.wrapping.style.marginTop = Math.floor((-newHeight + hh) / 2) + 'px';
+    open_var.button_close.style.width = open_var.footerBar.offsetHeight - 4 + 'px';
+    open_var.button_close.style.height = open_var.footerBar.offsetHeight - 4 + 'px';
+    open_var.button_like.style.width = open_var.footerBar.offsetHeight - 4 + 'px';
+    open_var.button_like.style.height = open_var.footerBar.offsetHeight - 4 + 'px';
+    open_var.button_dislike.style.width = open_var.footerBar.offsetHeight - 4 + 'px';
+    open_var.button_dislike.style.height = open_var.footerBar.offsetHeight - 4 + 'px';
+    open_var.button_share.style.width = open_var.footerBar.offsetHeight - 4 + 'px';
+    open_var.button_share.style.height = open_var.footerBar.offsetHeight - 4 + 'px';
+  } else {
+    //console.log('portrait'); //portrait
     newHeight = newWidth / open_var.widthToHeight;
-    open_var.wrapping.style.height = newHeight-coefHeight + 'px';
-    open_var.wrapping.style.width = newWidth-coefWidth + 'px';
+    open_var.wrapping.style.width = Math.floor(newWidth - hh) + 'px';
+    open_var.wrapping.style.height = Math.floor(newHeight - hh) + 'px';
+    open_var.imgPlace.style.width = Math.floor(newWidth - hh - hFooter) + 'px';
+    open_var.imgPlace.style.height = Math.floor(newHeight - hh - hFooter) + 'px';
+    open_var.wrapping.style.marginLeft = Math.floor((-newWidth + hh) / 2) + 'px';
+    open_var.wrapping.style.marginTop = Math.floor((-newHeight + hh) / 2) + 'px';
   }
-  open_var.wrapping.style.marginLeft = ((-newWidth+coefWidth)/ 2) + 'px';
-  open_var.wrapping.style.marginTop = ((-newHeight+coefHeight) / 2) + 'px';
-  append(open_var.headerBar);
-  append(open_var.footerBar);
-
-  var imgPlace = open_var.imgPlace;
-  imgPlace.style.width = newWidth-coefWidth+'px';
-  imgPlace.style.height = newHeight-coefHeight+'px';
 }
-//window.addEventListener('resize', resizeGame, false);
-//window.addEventListener('orientationchange', resizeGame, false);
-
-
-
-
-
-
-function addEvent( element, event, callback ) {
-   if( window.addEventListener ) {
-      element.addEventListener( event, callback, false );
-   } else if( document.attachEvent ) {
-      element.attachEvent( 'on' + event, callback );
-   } else {
-      element[ 'on' + event ] = callback;
-   }
+//window.addEventListener('resize', resizer, false);
+//window.addEventListener('orientationchange', resizer, false);
+function addEvent(element, event, callback) {
+  if (window.addEventListener) {
+    element.addEventListener(event, callback, false);
+  } else if (document.attachEvent) {
+    element.attachEvent('on' + event, callback);
+  } else {
+    element['on' + event] = callback;
+  }
 }
-addEvent(window, 'resize', resizeGame);
-addEvent(open_var.ig, 'click', function () {
-  this.style.boxShadow = 'inset 0 0 24px 3px rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')'
-});
-addEvent(open_var.ig, 'mouseover', function () {
-  this.style.cursor = 'pointer';
-  this.style.boxShadow = 'inset 0 0 24px 3px rgb(256,' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')'
-});
+addEvent(window, 'resize', resizer);
 /**/
 
 
--- test_ciklum_v1/js/scale_v1.js
+++ test_ciklum_v1/js/scale_v1.js
@@ -16,30 +16,30 @@ sizeModule = (function () {
          _arrWxH.push(window.innerWidth, window.innerHeight, window.innerWidth / window.innerHeight, 4 / 3);
          var arr =_arrWxH.splice(0, _arrWxH.length);
 
+         sizeModule.calculate(arr);
+      } ,
+      calculate: function (arr) {  //arr [W=320, H=440, W/H=0.7272727272727273, 4/3=1.3333333333333333]
          var noda;
-         var wx,  hx,  nw, nh;
-         nw = arr[0]/arr[3];
-         nh = arr[1]/arr[3];
-         wx = (arr[0] * 16) / 100;
-         hx = (arr[1] * 16) / 100;
+         var    nw, nh;
+         nw = Math.floor(arr[0]/arr[3]);
+         nh = Math.floor(arr[1]/arr[3]);
          noda = document.getElementById('wrapping');
-
-         if (arr[2] >= arr[3]) { console.log('landscape');  //landscape
-            noda.style.width= nw-wx+'px';
-            noda.style.height= nh-hx+'px';
+          var n = noda.style.width.slice(0,-2)/noda.style.height.slice(0,-2);
+         if (arr[2] >  arr[3]) {  console.log('landscape');  //landscape
+            noda.style.width= nw +'px';
+            noda.style.height= nh +'px';
          }
-         else {               console.log('portrait');     //portrait
+        else {              // console.log('portrait');     //portrait
+
+            noda.style.width= nw +'px';
+            noda.style.height= nh +'px';
             // newHeight = newWidth / open_var.widthToHeight;
             // open_var.wrapping.style.height = newHeight-coefHeight + 'px';
             // open_var.wrapping.style.width = newWidth-coefWidth + 'px';
-         }
+         } /**/
 
-         noda.style.marginLeft = ((-arr[0]+wx)/ 2) + 'px';
-         noda.style.marginTop = ((-arr[1]+hx)/ 2) + 'px';
-         //sizeModule.getItemCount(wh);
-      } ,
-      calculate: function (arr) {
-
+         noda.style.marginLeft = ((-arr[0] )/ 2) + 'px';
+         noda.style.marginTop = ((-arr[1] )/ 2) + 'px';
       },
       getItemCount: function (arr) {
          //console.log('wh= '+ arr);
