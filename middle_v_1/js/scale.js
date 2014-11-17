a9a58ed900cad7c640059edbd3fd68069565b6a4
BikTop <mantorovv@gmail.com>
2014-11-17 23:24:47 +0200

    tempplate1

---
+++ middle_v_1/js/scale.js
@@ -0,0 +1,35 @@
+/**
+ * Created by BikTop on 08.11.2014.
+ */
+window.onload=(function(){
+});
+
+
+function resizeGame() {
+  var gameArea = document.querySelector('.container');
+  var widthToHeight = 4 / 3;
+  var newWidth = window.innerWidth;
+  var newHeight = window.innerHeight;
+  var newWidthToHeight = newWidth / newHeight;
+
+  if (newWidthToHeight > widthToHeight) {
+    newWidth = newHeight * widthToHeight;
+    gameArea.style.height = newHeight + 'px';
+    gameArea.style.width = newWidth + 'px';
+  } else {
+    newHeight = newWidth / widthToHeight;
+    gameArea.style.width = newWidth + 'px';
+    gameArea.style.height = newHeight + 'px';
+  }
+
+  gameArea.style.marginTop = (-newHeight / 2) + 'px';
+  gameArea.style.marginLeft = (-newWidth / 2) + 'px';
+
+  var gameCanvas = document.querySelector('.container');
+  gameCanvas.width = newWidth;
+  gameCanvas.height = newHeight;
+}
+
+
+window.addEventListener('resize', resizeGame, false);
+window.addEventListener('orientationchange', resizeGame, false);
