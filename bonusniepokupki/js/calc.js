7199981bbde5b338c194a3c4b7a0ae8475630fc5
MantorovVv <Viktor.Mantorov@creditdnepr.com>
2014-12-19 18:04:44 +0200

    works

--- bonusniepokupki/js/calc.js
+++ bonusniepokupki/js/calc.js
@@ -6,163 +6,124 @@
             'class_child_g': 'sprite_img',
             'travel': 'air_travel',
             'bonuses': 'bonuses',
-            'fill': {'width':'1%'},
+            'fill': {'width': '1%'},
             'clasName': '',
+            'scaleValue': [0, 500, 1000, 1500, 2000, 2500, 0, 1000, 2000, 3000, 4000, 5000],
             'restoran': 'restoran',
-            'scale_I': [3000, 10000, 50000],
-            'scale_II': [4500, 15000, 75000],
             'text_travel': 'Банк оплачивает авиабилеты в сумме, грн:',
             'text_bonuses': 'БАЛАНС БАЛЛОВ:',
             'text_restoran': 'БАНК ОПЛАЧИВАЕТ ПОСЕЩЕНИЕ В РЕСТОРАНЫ В СУММЕ, ГРН:',
             'maxNum': [1, 0, 0, 0, 0, 0],
             'town_activ': ['0 0', '-86px 0', '-172px 0', '-258px 0', '-344px 0', '-430px 0'],
-            'town_lock':  ['0 -66px', '-86px -66px', '-172px -66px', '-258px -66px', '-344px -66px', '-430px -66px'],
-            'txt':  ['praga', 'polsha', 'london', 'pari', 'greec', 'dubai', 'barselona'],
-            noda:function(a){
-               //$(a).css({'width':'83px'})
-               //console.log($('li').css({'background':'#f0f'}) );
-               //console.log(a.css({'background':'#f0f'}) );
-                      as = $(a).parent()
-              // a.style.width='20px'
-              // console.log( a);
-               this.width()
+            'town_lock': ['0 -66px', '-86px -66px', '-172px -66px', '-258px -66px', '-344px -66px', '-430px -66px'],
+            'txt': ['praga', 'polsha', 'london', 'pari', 'greec', 'dubai', 'barselona'],
+            noda: function (a) {
+               //console.log($('.face_scale >ul'));
+               var margin = ((529 / this.maxNum.length) - 83) + 'px';
+               a.style.marginRight = margin;
             },
-            width:function(){
-                var a=529;
-               var b= this.maxNum.length;
-                var margin=((a/b)-83)+'px';
-                console.log(margin);
-            },
-            clasCreat: function (i) {
+            clasCreat: function (i) {            //возвращает клас в формате sprite_img box2 если требуется добовляет active
                var cass, act;
-               (this.maxNum[i]) ? act = 'active' : act = ''; 
+               (this.maxNum[i]) ? act = 'active' : act = '';
                cass = this.class_child_g + ' ' + 'box' + (i + 1) + ' ' + act;
 
                return cass;
             },
 
-            li_li: function () {
+            li_li: function () {                //исходя из значения 'maxNum' длина масива- кол-во городов 1- вкл., -1-выкл., 0-hold
                var arr = [], i = 0,
                   cla = [],
                   posLock = [],
                   posAct = [];
-
-
                for (; i < this.maxNum.length; i += 1) {
 
-                  if(this.maxNum[i] === 1){
+                  if (this.maxNum[i] === 1) {
                      cla.push(this.clasCreat(i));
-                     arr.push( this.createEl('li', cla.shift(), {'background-position': this.town_activ[i],
-                           //'margin': Math.float( ( ($(this).parent().width()-83)*6)/6)+'px'
-                           } ).
-                        prepend(
-                           this.createEl('div','tex').text(this.txt[i])
+                     arr.push(this.createEl('li', cla.shift(), {'background-position': this.town_activ[i]}).
+                           prepend(
+                           this.createEl('div', 'tex').text(this.txt[i])
                         )
-                     ) ;  
+                     );
                   }
                   else if (this.maxNum[i] === 0) {
                      cla.push(this.clasCreat(i));
                      arr.push(
                         this.createEl('li', cla.shift()).
                            css(
-                              {'background-position': this.town_lock[i]}
+                           {'background-position': this.town_lock[i]}
                         ).prepend(
-                           this.createEl('div','tex').text(this.txt[i])
+                           this.createEl('div', 'tex').text(this.txt[i])
                         )
                      );
                   }
-                  else if(this.maxNum[i] === -1){
-                     arr.push( this.createEl('li', '', {'background-position': 'none'}).prepend(
-                        this.createEl('div','tex').text()
-                     ) )
-
+                  else if (this.maxNum[i] === -1) {
+                     arr.push(this.createEl('li', '', {'background-position': 'none'}).prepend(
+                        this.createEl('div', 'tex').text()
+                     ))
                   }
+
                   posLock.push(this.town_lock[i]);
                   posAct.push(this.town_activ[i]);
                }
-
+               console.log($(arr[0]));
                return arr;
             },
 
-            scale:function(options){
-
-               var arr =[];
-               if(options!== 'bonuses'){
-                  arr.push( this.createEl('div', 'scale').
+            scale: function (options) {
+               var arr = [];
+               if (options !== 'bonuses') {
+                  arr.push(this.createEl('div', 'scale').
                         prepend(this.createEl('div', options).
-                           append(this.grid ).
-                           prepend( this.createEl('div', 'fill', this.fill))
-                        )
+                           append(this.grid).
+                           prepend(this.createEl('div', 'fill', this.fill))
+                     )
                   );
                }
-               else{
-                  arr.push( this.createEl('div', options).
-                        prepend( this.createEl('div', 'fill', this.fill))
+               else {
+                  arr.push(this.createEl('div', options).
+                        prepend(this.createEl('div', 'fill', this.fill))
                   );
                }
-
                return arr;
             },
-
-            createEl:function(el, clasName, param){
-               var e, noda;
+            createEl: function (el, clasName, param) {
+               var e;
                (param) ? e = $('<' + el + '></' + el + '>').addClass(clasName).css(param) : e = $('<' + el + '></' + el + '>').addClass(clasName);
-               if(el==='li'){
-                  noda=e[0];
-                  this.noda( noda )
-               }
-                return e;
+               (el === 'li') ? this.noda(e[0]) : '';
+               (clasName === 'grid') ? e.css({'width': ((100 / this.maxNum.length) + '%')}) : '';
+               return e;
             },
-
             grid: function () {
-               var arr = [], i = 0;
-
-               for (; i < settings.maxNum.length; i += 1) {   //console.log(num[i]);
-                  arr.push( settings.createEl('div', 'grid').append(settings.spans) )
+               var arr = [],
+                  i = 0;
+               for (; i < settings.maxNum.length; i += 1) {
+                  arr.push(settings.createEl('div', 'grid').append(settings.spans))
                }
                return arr;
             },
-            spans:function(){
-               var ar=[];
+
+            spans: function () {       //5 полосок мм, в output вставим цифры
+               var ar = [],as,
+                  scaleValue = settings.scaleValue.shift();
                for (var i = 0; i < 5; i += 1) {
-                  var t=i;
-                  if(i==0){
-                     ar.push(settings.createEl('div', 'output').text(settings.forin(settings.scale_I[0])));
-                  }
-                  ar.push(settings.createEl('span'));
+                  as='a'+i
+                  console.log(as);
+                  (i == 0) ? ar.push(settings.createEl('div', 'output').text(scaleValue)) : '';
+                  ar.push(settings.createEl('span', as));
                }
-               //ar[0].css({'background':'#f0f'})
-               //console.log( ar[0].parent() );
                return ar;
             },
-            forin:function(ell){
-                    var i=0;
-               function a(ell){
-                 // console.log(ell);
-                   i+=1;
-                  //console.log(i);
-                  return (ell/5) ;
-               }
-               a(ell);
-            }
-
-
          }, options);
-
          return this.each(function () {
             $(this).prepend('<h4>' + settings.text_travel + '</h4>',
-               $('<ul></ul>').prepend(settings.li_li()).append( settings.scale(settings.travel), '<h4>' + settings.text_bonuses + '</h4>').append( settings.scale(settings.bonuses), '<h4>' + settings.text_restoran + '</h4>').append( settings.scale(settings.restoran))
+               $('<ul></ul>').prepend(settings.li_li()).append(settings.scale(settings.travel), '<h4>' + settings.text_bonuses + '</h4>').append(settings.scale(settings.bonuses), '<h4>' + settings.text_restoran + '</h4>').append(settings.scale(settings.restoran))
             );
          });
-
       },
       remowe: function () {
          $(this).remove();
       }
    };
-
-
-
    $.fn.tooltip = function (method) {
       if (methods[method]) {
          return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
@@ -176,12 +137,13 @@
 window.onload = function () {
    $('.face_scale>h4').tooltip('remowe');
    $('.face_scale>ul').tooltip('remowe');
-   $('.face_scale').tooltip( { 'maxNum': [1,1, 0, 0, 0, 1],
+   $('.face_scale').tooltip({
+      'maxNum': [-1, 1, 0, 0, 0, 0],
       'travel': 'air_travel',
-      'fill': {'width':'0%'},
+      'fill': {'width': '0%'},
       'bonuses': 'bonuses',
-      'restoran': 'restoran'
-   } );
-
+      'restoran': 'restoran',
+      'scaleValue': [2002, 501, 1000, 1500, 2000, 2001, 2002, 4002, 1000, 2000, 3000, 4000, 4001, 4002]
+   });
 };
 
